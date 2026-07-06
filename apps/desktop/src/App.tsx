c848f2cf968aaa75b96fbe86e1417a2bfdef9f42
Maddie D. Reese <maddie@maddiedreese.com>
2026-07-06 10:24:11 -0700

    Extract room panel action wiring

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -45,7 +45,7 @@ import { useAppViewProps } from "./hooks/useAppViewProps";
 import { useWorkspaceFlowContext } from "./hooks/useWorkspaceFlowContext";
 import { useRelaySyncContext } from "./hooks/useRelaySyncContext";
 import { useRoomRuntimeContext } from "./hooks/useRoomRuntimeContext";
-import { useRoomPanelActions } from "./hooks/useRoomPanelActions";
+import { useAppRoomPanelActions } from "./hooks/useAppRoomPanelActions";
 import { InlineSecretWarning } from "./components/common";
 import { AppShellView } from "./components/AppShellView";
 import type { InspectorTab } from "./components/RoomInspectorPanel";
@@ -968,64 +968,14 @@ export function App() {
     }
   });
 
-  const roomPanels = useRoomPanelActions({
-    chat: {
-      selectedRoomId: selectedRoom.id,
-      messages,
-      localPreviews: selectedRuntime.localPreviews,
-      copyMessageMarkdown: workspaceFlow.copyMessageMarkdown,
-      copyCodexOutputMarkdown: workspaceFlow.copyCodexOutputMarkdown,
-      openEncryptedAttachmentBlob: workspaceFlow.openEncryptedAttachmentBlob,
-      toggleMessageReaction: roomInteraction.toggleMessageReaction,
-      setPendingCodexApprovalForRoom,
-      setApprovalVisibleForRoom,
-      approveCodexTurn: roomRuntime.approveCodexTurn,
-      handleCodexInvoke: roomRuntime.handleCodexInvoke,
-      copyMarkdownWithFallback: workspaceFlow.copyMarkdownWithFallback,
-      setChatMessageForRoom,
-      stopLocalPreview: roomRuntime.stopLocalPreview,
-      setDraftForRoom
-    },
-    header: {
-      rooms: workspaceState.rooms,
-      selectedRoomId: workspaceState.selectedRoomId,
-      selectedRoomIdForTabs: selectedRoom.id,
-      activeBrowserUrl,
-      setSelectedTeam: workspaceState.setSelectedTeam,
-      setSelectedRoomId: workspaceState.setSelectedRoomId,
-      setInspectorTabsByRoom: roomRuntimeState.setInspectorTabsByRoom,
-      openRoomBrowserNow: roomRuntime.openRoomBrowserNow
-    },
-    terminal: {
-      selectedRoomId: selectedRoom.id,
-      terminalRequests: selectedRuntime.terminalRequests,
-      copyTerminalMarkdown: workspaceFlow.copyTerminalMarkdown,
-      runApprovedTerminalCheck: roomRuntime.runApprovedTerminalCheck,
-      openInteractiveTerminal: roomRuntime.openInteractiveTerminal,
-      setTerminalNameForRoom,
-      setTerminalCommandForRoom,
-      startNamedTerminal: roomRuntime.startNamedTerminal,
-      requestTerminalCommand: roomRuntime.requestTerminalCommand,
-      approveTerminalRequest: roomRuntime.approveTerminalRequest,
-      denyTerminalRequest: roomRuntime.denyTerminalRequest,
-      setSelectedTerminalIdForRoom,
-      setTerminalInputForRoom,
-      sendTerminalInput: roomRuntime.sendTerminalInput,
-      restartSelectedTerminal: roomRuntime.restartSelectedTerminal,
-      stopSelectedTerminal: roomRuntime.stopSelectedTerminal
-    },
-    workspaceFiles: {
-      selectedRoomId: selectedRoom.id,
-      copyProjectMarkdown: workspaceFlow.copyProjectMarkdown,
-      setFileQueryForRoom,
-      openProjectFile: workspaceFlow.openProjectFile,
-      copyDiffSummaryMarkdown: workspaceFlow.copyDiffSummaryMarkdown,
-      attachSelectedFileToMessage: workspaceFlow.attachSelectedFileToMessage,
-      setFilePreviewTabForRoom,
-      setSelectedFileForRoom,
-      setSelectedDiffForRoom,
-      setSensitiveAttachmentReviewKey: roomChatState.setSensitiveAttachmentReviewKey
-    }
+  const roomPanels = useAppRoomPanelActions({
+    appState,
+    selected: selectedContext,
+    selectedRuntime,
+    roomInteraction,
+    roomSetters,
+    roomRuntime,
+    workspaceFlow
   });
   const appView = useAppViewProps({
     shell: {
